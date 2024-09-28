7de33685a2599e02fc7ded59b40ea3b0878bf8e2
Zaki Rangwala <zakirangwala@gmail.com>
2024-09-28 11:49:01 -0400

    init scraper

---
+++ scraper/main.py
@@ -0,0 +1,23 @@
+cities = [
+    "Agincourt", "Alderwood", "Amesbury Park", "Annex", "Armour Heights", "Bayview Village", 
+    "Beach", "Bendale", "Birch Cliff", "Bloor West Village", "Bridle Path-Sunnybrook", "Brockton",
+    "Cabbagetown", "Cedarvale", "Clairlea-Golden Mile", "Cliffside-Cliffcrest", "Danforth", 
+    "Deer Park", "Don Mills", "Don Valley Village", "Dorset Park", "Dovercourt", "Downsview", 
+    "Downtown", "Dufferin Grove-Bickford Park", "Earlscourt", "East York", "Eatonville", 
+    "Elia-Branson", "Emery", "Eringate", "Exhibition", "Fairbank", "Financial District", 
+    "Flemingdon Park", "Forest Hill", "Garrison", "Guildwood", "Harbour", "Henry Farm", 
+    "High Park", "Highland Creek", "Hillcrest Village", "Humber Bay", "Humber Summit", 
+    "Humber Valley", "Islington", "Junction", "Kensington-Grange", "King-Spadina", "Kingsway", 
+    "Knob Hill", "Lambton", "L’Amoreaux", "Lansing", "Lawrence Manor", "Leaside", "Leslieville", 
+    "Long Branch", "Malvern", "Maple Leaf", "Maryvale", "Milliken", "Mimico", "Mount Dennis", 
+    "New Toronto", "Newtonbrook", "North Toronto", "Oakridge", "Oakwood-Vaughan", 
+    "Palmerston-Sussex Ulster", "Parkdale", "Pleasant View", "Port Lands", "Port Union-West Rouge", 
+    "Railway Lands", "Regent Park", "Rexdale", "Richview", "Riverdale", "Rosedale-Moore Park", 
+    "Rouge", "Scarborough Junction", "Scarborough Village", "Seaton Village", "Silverthorn", 
+    "St. James Town", "St. Lawrence", "Sunnylea", "Sunnyside", "Swansea", "Thistletown", 
+    "Thorncliffe Park", "Toronto Islands", "Trinity Bellwoods", "University", "Upper Beach", 
+    "Upper Rouge", "West Hill", "Weston", "Wexford", "Willowdale", "Woburn", "Wychwood-Hillcrest", 
+    "York Mills", "York University-Black Creek", "Yorkville"
+]
+
+print()
