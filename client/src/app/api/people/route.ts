e60340be446e000790603cbd3e7721d2586f95b7
Zaki Rangwala <zakirangwala@gmail.com>
2024-09-29 01:47:47 -0400

    add more data to endpoint

--- client/src/app/api/people/route.ts
+++ client/src/app/api/people/route.ts
@@ -1,10 +1,16 @@
 // fetch from 10,000+ images of people -> every time you reload you will get a new memory made by a person. 
 // the memories in toronto are so rich in culture and history.
 
+// connecting new with old, because we create an instagram like experience if people from then could share pictures
+
+// structure : primaryMaker (poster), primaryMedia(media), title (caption), date (date)
+// if no date then pick a random date from 1955-1986
+
+// Elton John : https://digitalarchive.tpl.ca/internal/media/dispatcher/1995177/full
 export async function GET() {
   // Fetch the JSON data from the given URL
   const res = await fetch(
-    "https://digitalarchive.tpl.ca/search/*/objects/json?filter=classifications%3APicture%3BsubjectThesFilter%3Ahttp%25255C%3A%252F%252Fnodes.emuseum.com%252FY8SQECAV%252Fapis%252Femuseum%252Fnetwork%252Fv1%252Fvocabularies%252FtermMaster6713006",
+    "https://digitalarchive.tpl.ca/search/*/objects/json?filter=classifications%3APicture%3BsubjectThesFilter%3Ahttp%25255C%3A%252F%252Fnodes.emuseum.com%252FY8SQECAV%252Fapis%252Femuseum%252Fnetwork%252Fv1%252Fvocabularies%252FtermMaster6718023",
     {
       headers: {
         "Content-Type": "application/json",
