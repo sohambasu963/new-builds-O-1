250febc105e749ed57f9356eaf0be08423737df7
Zaki Rangwala <zakirangwala@gmail.com>
2024-09-29 01:35:57 -0400

    infinite reload images

---
+++ client/src/app/api/people/route.ts
@@ -0,0 +1,35 @@
+// fetch from 10,000+ images of people -> every time you reload you will get a new memory made by a person. 
+// the memories in toronto are so rich in culture and history.
+
+export async function GET() {
+  // Fetch the JSON data from the given URL
+  const res = await fetch(
+    "https://digitalarchive.tpl.ca/search/*/objects/json?filter=classifications%3APicture%3BsubjectThesFilter%3Ahttp%25255C%3A%252F%252Fnodes.emuseum.com%252FY8SQECAV%252Fapis%252Femuseum%252Fnetwork%252Fv1%252Fvocabularies%252FtermMaster6713006",
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  // Parse the JSON data
+  const data = await res.json();
+
+  // Check if the results array exists and has data
+  if (data.count > 0 && data.results.length > 0) {
+    // Randomly select an object from the results array
+    const randomIndex = Math.floor(Math.random() * data.results.length);
+    const randomObject = data.results[randomIndex];
+
+    // Return the random object as a JSON response
+    return new Response(JSON.stringify({ data: randomObject }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } else {
+    // Return an error if no results are found
+    return new Response(JSON.stringify({ error: "No results found." }), {
+      headers: { "Content-Type": "application/json" },
+      status: 404,
+    });
+  }
+}
